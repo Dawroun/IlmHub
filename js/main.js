694b4114efffc150a0fa3b70aade0213094da7c3
Davron Abdukhakimov <davron2806@gmail.com>
2021-11-15 03:00:32 +0500

    screenshot section done

--- js/main.js
+++ js/main.js
@@ -46,5 +46,25 @@ $(document).ready(function () {
         }
     })
 
+    /*------------------ Screenshot Carousel -----------------*/
+
+    $('.screenshots-carousel').owlCarousel({
+        loop: true,
+        margin: 0,
+        autoplay: true,
+        responsiveClass: true,
+        responsive: {
+            0: {
+                items: 1,
+            },
+            600: {
+                items: 2,
+            },
+            1000: {
+                items: 4,
+            }
+        }
+    })
+
 
 });
