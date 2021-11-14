95b147216d602b3c3d95952339a62a56d76240d8
Davron Abdukhakimov <davron2806@gmail.com>
2021-11-15 02:04:55 +0500

    features section done

--- js/main.js
+++ js/main.js
@@ -1,4 +1,15 @@
 $(document).ready(function () {
+    /*------------------ Navbar Shrink-----------------*/
+    $(window).on("scroll", function () {
+        if ($(this).scrollTop() > 90) {
+            $(".navbar").addClass("navbar-shrink");
+        }
+        else {
+            $(".navbar").removeClass("navbar-shrink");
+        }
+    })
+
+
     /*------------------ Video Popup -----------------*/
     const videoSrc = $("#player-1").attr("src");
     $(".video-play-btn, .video-popup").on("click", function () {
@@ -13,5 +24,27 @@ $(document).ready(function () {
             }
 
         }
+    });
+
+    /*------------------ Features Carousel -----------------*/
+
+    $('.features-carousel').owlCarousel({
+        loop: true,
+        margin: 0,
+        autoplay: true,
+        responsiveClass: true,
+        responsive: {
+            0: {
+                items: 1,
+            },
+            600: {
+                items: 2,
+            },
+            1000: {
+                items: 3,
+            }
+        }
     })
-})
+
+
+});
