292da6c0e323805ee4b5f186ca80af8742d76fc5
Davron Abdukhakimov <davron2806@gmail.com>
2021-11-17 01:24:32 +0500

    added testimonials section

--- js/main.js
+++ js/main.js
@@ -66,5 +66,25 @@ $(document).ready(function () {
         }
     })
 
+    /*------------------ Testimonials Carousel -----------------*/
+
+    $('.testimonials-carousel').owlCarousel({
+        loop: true,
+        margin: 0,
+        autoplay: true,
+        responsiveClass: true,
+        responsive: {
+            0: {
+                items: 1,
+            },
+            600: {
+                items: 2,
+            },
+            1000: {
+                items: 4,
+            }
+        }
+    })
+
 
 });
