4d205683a987cb23bc3b0ba54c024f9c313e3170
Davron Abdukhakimov <davron2806@gmail.com>
2021-11-18 21:26:50 +0500

    another commit

--- js/main.js
+++ js/main.js
@@ -81,7 +81,7 @@ $(document).ready(function () {
                 items: 2,
             },
             1000: {
-                items: 4,
+                items: 3,
             }
         }
     })
